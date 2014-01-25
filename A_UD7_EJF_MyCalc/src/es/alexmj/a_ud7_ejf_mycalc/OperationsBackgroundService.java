fcbc77518b7a5af8adf8c967bfb29ac7c0e84522
WitchArroW <alejandro.marijuan@googlemail.com>
2014-01-25 14:11:45 +0100

    Creación inicial proyecto.

---
+++ A_UD7_EJF_MyCalc/src/es/alexmj/a_ud7_ejf_mycalc/OperationsBackgroundService.java
@@ -0,0 +1,16 @@
+package es.alexmj.a_ud7_ejf_mycalc;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+
+public class OperationsBackgroundService extends Service{
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
